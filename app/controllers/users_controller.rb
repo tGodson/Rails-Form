f79a16d37edc7b0c860ef348010c23aea4beff63
Ignatius22 <igee54077@gmail.com>
2020-07-31 01:52:39 +0100

    make change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
   
   def create
-  	# @user = User.new(username: params[:username], email: params[:email], password: params[:password])
   	@user = User.new(user_params)
   	if @user.save
   		redirect_to new_user_path
